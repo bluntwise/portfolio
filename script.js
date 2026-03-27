730fb4008ff610ee6ad923d14981ed96e54ddbdb
Alan DELY <114935902+bluntwise@users.noreply.github.com>
2026-03-27 12:17:00 +0100

    [ADD] switch language feature

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 function switchLanguage() {
   const currentPage = window.location.pathname;
   
-  if (currentPage.includes('index-en.html')) {
+  // Récupère le chemin relatif (fonctionne avec GitHub Pages et localhost)
+  const isEnglish = currentPage.includes('index-en.html');
+  
+  if (isEnglish) {
     // On est en anglais, go to français
-    window.location.href = window.location.pathname.replace('index-en.html', 'index-fr.html');
+    const newPath = currentPage.replace('index-en.html', 'index-fr.html');
+    window.location.href = newPath;
   } else {
     // On est en français (ou root), go to anglais
-    if (currentPage.endsWith('index-fr.html') || currentPage.endsWith('/')) {
-      window.location.href = window.location.pathname.replace('index-fr.html', 'index-en.html').replace(/\/$/, '/index-en.html');
+    if (currentPage.endsWith('index-fr.html')) {
+      const newPath = currentPage.replace('index-fr.html', 'index-en.html');
+      window.location.href = newPath;
+    } else if (currentPage.endsWith('/') || currentPage.endsWith('/portfolio/')) {
+      // Si on est à la racine, redirige vers la version anglaise
+      const newPath = currentPage + (currentPage.endsWith('/') ? '' : '/') + 'index-en.html';
+      window.location.href = newPath;
     }
   }
 }
